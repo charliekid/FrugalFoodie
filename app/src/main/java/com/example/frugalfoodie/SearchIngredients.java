3294980bf6f14b795e2fda9900e8adde82e1224a
Cathy Hsu <chsucathy@gmail.com>
2020-11-11 17:56:02 -0800

    Set up Ingredient Recycler View xml file

---
+++ app/src/main/java/com/example/frugalfoodie/SearchIngredients.java
@@ -0,0 +1,14 @@
+package com.example.frugalfoodie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchIngredients extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_ingredients);
+    }
+}
