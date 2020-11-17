63750f8e290c6d7c667aec7e65d92eece8daece8
Emory Schmitt <emory.m.schmitt@gmail.com>
2020-11-16 22:41:24 -0800

    Dont Merge ! frontend done but error in IngredientAdapter

---
+++ app/src/main/java/com/example/frugalfoodie/Adapter/RecipeAdapter.java
@@ -0,0 +1,71 @@
+package com.example.frugalfoodie.Adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.frugalfoodie.DB.Recipe;
+
+import com.example.frugalfoodie.databinding.ActivityViewRecipesBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Citation:
+// https://www.java67.com/2014/06/how-to-format-float-or-double-number-java-example.html
+// https://stackoverflow.com/questions/51515854/check-all-the-check-boxes-in-recycler-view
+// https://stackoverflow.com/questions/33434626/get-list-of-checked-checkboxes-from-recyclerview-android
+// https://stackoverflow.com/questions/33316837/how-to-prevent-items-from-getting-duplicated-when-scrolling-recycler-view
+public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecipeViewHolder> {
+
+        private List<Recipe> recipes;
+        private ActivityViewRecipesBinding viewRecipeBinding;
+
+
+        public RecipeAdapter(ArrayList<Recipe> allRecipes, ArrayList<Recipe> recipes) {
+            this.recipes = recipes;
+        }
+        @NonNull
+        @Override
+        public RecipeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+            viewRecipeBinding = viewRecipeBinding.inflate(inflater);
+            View view = viewRecipeBinding.getRoot();
+            return new RecipeViewHolder(view);
+        }
+
+
+    @Override
+        public void onBindViewHolder(@NonNull RecipeViewHolder holder, int position) {
+            final Recipe currentRecipe = recipes.get(position);
+           // viewRecipeBinding.recipeCheckbox.setText(currentRecipe.getRecipeName());
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return recipes.size();
+        }
+
+        // getItemId and getItemViewType will prevent recyclerView from duplicating ingredient items
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            return position;
+        }
+
+
+    class RecipeViewHolder extends RecyclerView.ViewHolder {
+
+            public RecipeViewHolder(@NonNull View itemView) {
+                super(itemView);
+            }
+        }
+    }
--- app/src/main/java/com/example/frugalfoodie/ViewRecipes.java
+++ app/src/main/java/com/example/frugalfoodie/ViewRecipes.java
@@ -1,10 +1,44 @@
 package com.example.frugalfoodie;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.example.frugalfoodie.Adapter.RecipeAdapter;
+import com.example.frugalfoodie.DB.FFRoom;
+import com.example.frugalfoodie.DB.Recipe;
+import com.example.frugalfoodie.databinding.ActivityViewRecipesBinding;
+
+import java.util.ArrayList;
 
 public class ViewRecipes extends AppCompatActivity {
+    private FFRoom db;
+    private ArrayList<Recipe> recipes;
+    private ActivityViewRecipesBinding activityViewRecipesBinding;
 
-    //static List<>
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activityViewRecipesBinding = ActivityViewRecipesBinding.inflate(getLayoutInflater());
+        View view = activityViewRecipesBinding.getRoot();
+        setContentView(view);
+
+        db = FFRoom.getInstance(getApplicationContext());
+       recipes = new ArrayList<>();
+
+        ArrayList<Recipe> allRecipes = (ArrayList<Recipe>) db.recipeDAO().getAllRecipes();
+        RecipeAdapter adapter = new RecipeAdapter(allRecipes, recipes);
+
+
+        activityViewRecipesBinding.RecipeRecyclerView.setAdapter(adapter);
+        activityViewRecipesBinding.RecipeRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+
+    }
 
 
 
