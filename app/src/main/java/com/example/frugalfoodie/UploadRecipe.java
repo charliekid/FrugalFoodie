29a5178a9e60dd819239b88f3d10b62f91f0293f
Cathy Hsu <chsucathy@gmail.com>
2020-11-27 14:15:35 -0800

    Set up corresponding Java class

--- app/src/main/java/com/example/frugalfoodie/UploadRecipe.java
+++ app/src/main/java/com/example/frugalfoodie/UploadRecipe.java
@@ -3,12 +3,21 @@ package com.example.frugalfoodie;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.frugalfoodie.DB.FFRoom;
+import com.example.frugalfoodie.databinding.ActivityUploadRecipeBinding;
 
 public class UploadRecipe extends AppCompatActivity {
 
+    private ActivityUploadRecipeBinding activityUploadRecipeBinding;
+    private FFRoom db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_upload_recipe);
+        db = FFRoom.getInstance(getApplicationContext());
+        activityUploadRecipeBinding = ActivityUploadRecipeBinding.inflate(getLayoutInflater());
+        View view = activityUploadRecipeBinding.getRoot();
+        setContentView(view);
     }
 }
