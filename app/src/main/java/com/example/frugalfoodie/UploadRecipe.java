c985d471d030fb8b96ce241e29d1cfafe6167a0a
Cathy Hsu <chsucathy@gmail.com>
2020-11-27 14:00:55 -0800

    Create Upload Recipe activity

--- app/src/main/java/com/example/frugalfoodie/UploadRecipe.java
+++ app/src/main/java/com/example/frugalfoodie/UploadRecipe.java
@@ -2,5 +2,13 @@ package com.example.frugalfoodie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+
 public class UploadRecipe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_recipe);
+    }
 }
