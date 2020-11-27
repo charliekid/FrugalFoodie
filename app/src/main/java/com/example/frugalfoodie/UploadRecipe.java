21cb95bd620d18b7cff4cf19a970f3e434c8ad50
Cathy Hsu <chsucathy@gmail.com>
2020-11-27 14:23:15 -0800

    Set up the frontend of upload recipe (#25)
    * Set up item_ingredient.xml which will render each ingredient view
    
    * Increase room version number
    
    * Set up Ingredient Recycler View xml file
    
    * Check if ingredients are being retrieved from database
    
    * Add search button to Search By Ingredients screen
    
    * Temporarily hardcode ingredients into database
    
    * Create IngredientAdapter
    
    * Display ingredient price so it is in price format
    
    * Save attempt to gather ingredients that have all been checked
    
    * Store all ingredients that have been checked
    
    * Create Upload Recipe activity
    
    * Finish creating xml file
    
    * Set up corresponding Java class

--- app/src/main/java/com/example/frugalfoodie/UploadRecipe.java
+++ app/src/main/java/com/example/frugalfoodie/UploadRecipe.java
@@ -2,5 +2,22 @@ package com.example.frugalfoodie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.frugalfoodie.DB.FFRoom;
+import com.example.frugalfoodie.databinding.ActivityUploadRecipeBinding;
+
 public class UploadRecipe extends AppCompatActivity {
+
+    private ActivityUploadRecipeBinding activityUploadRecipeBinding;
+    private FFRoom db;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        db = FFRoom.getInstance(getApplicationContext());
+        activityUploadRecipeBinding = ActivityUploadRecipeBinding.inflate(getLayoutInflater());
+        View view = activityUploadRecipeBinding.getRoot();
+        setContentView(view);
+    }
 }
